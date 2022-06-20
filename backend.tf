aa6be7e7bacff0e16d7b1ac48aaab4476abdf393
Jan Souza <jangs@br.ibm.com>
2022-06-20 09:52:23 -0300

    Change storage account

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate"
-    storage_account_name = "tfstate20838"
+    storage_account_name = "tfstate12783"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
