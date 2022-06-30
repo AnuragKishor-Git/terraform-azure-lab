cb74cbcf445f0d550ad270856e279b52401ce477
Jan Souza <jangs@br.ibm.com>
2022-06-30 09:25:19 -0300

    Update terraform storage

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate"
-    storage_account_name = "tfstate13204"
+    storage_account_name = "tfstate10908"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
