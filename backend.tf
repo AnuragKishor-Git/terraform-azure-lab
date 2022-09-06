0997c98776a97f19da8d2df9f0b8b00bf37b7ca1
Jan Souza <jangs@kyndryl.com>
2022-09-06 15:11:01 -0300

    Update storate account

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate"
-    storage_account_name = "tfstate27177"
+    storage_account_name = "tfstate20975"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
