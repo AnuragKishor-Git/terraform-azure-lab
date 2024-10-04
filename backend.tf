a711cada9d6b48d1344d6555f9df91a6c6d14694
githubActions <anu24may@outlook.com>
2024-10-05 01:24:13 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,1 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "test1-rg"
-    storage_account_name = "terraformazureak"
-    container_name       = "terraform"
-    key                  = "terraform.tfstate"
-  }
-}
+
